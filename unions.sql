94240d48914f91c8e993a0f06abf8d450596259f
r8ye <162093627+r8ye@users.noreply.github.com>
2026-05-19 16:38:04 +0800

    union distinct, all, use cases

---
+++ unions.sql
@@ -0,0 +1,70 @@
+SELECT *
+FROM employee_demographics
+UNION
+SELECT *
+FROM employee_salary;
+
+SELECT first_name,
+last_name
+FROM employee_demographics
+UNION
+SELECT salary,
+occupation
+FROM employee_salary;
+
+SELECT first_name,
+last_name
+FROM employee_demographics
+UNION DISTINCT
+SELECT first_name,
+last_name
+FROM employee_salary;
+
+SELECT first_name,
+last_name
+FROM employee_demographics
+UNION ALL
+SELECT first_name,
+last_name
+FROM employee_salary;
+
+
+SELECT first_name,
+last_name,
+'Old Man' AS Label
+FROM employee_demographics
+WHERE age >= 50 AND gender = 'male'
+UNION
+SELECT first_name,
+last_name,
+'Old Lady' AS Label
+FROM employee_demographics
+WHERE age >= 40 AND gender = 'female'
+UNION
+SELECT first_name,
+last_name,
+'Highly Paid' AS Label
+FROM employee_salary
+WHERE salary >= 70000
+ORDER BY first_name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
